f36b05fbd311669c6a1cb9a685efde318f690d34
Piorosen <chacha@udon.party>
2023-01-02 08:12:03 +0000

    add msd

--- lib/inc/scheduler/scheduler.h
+++ lib/inc/scheduler/scheduler.h
@@ -1,23 +1,14 @@
 #pragma once
 
-#include <scheduler/efts.h>
 #include <scheduler/fcfs.h>
-#include <scheduler/sjf.h>
-#include <scheduler/hrn.h>
 #include <scheduler/round_robine.h>
-#include <scheduler/random_sche.h>
 #include <memory>
 
 
 class scheduler { 
 private:
-    efts eft;
     fcfs fcf;
-    sjf sj;
-    hrn hrns;
     round_robine rr;
-    random_sche rss;
-    
     
 public:
     static scheduler* instance() { 
@@ -25,29 +16,12 @@ public:
         return &self;
     }
 
-    ischeduler* efts_scheduler() {
-        spdlog::info("efts_scheduler");
-        return &eft;
-    }
 
     ischeduler* fcfs_scheduler() {
         spdlog::info("fcfs_scheduler");
         return &fcf;
     }
     
-    ischeduler* sjf_scheduler() {
-        spdlog::info("sjf_scheduler");
-        return &sj;
-    }
-
-    ischeduler* hrn_scheduler() {
-        spdlog::info("hrn_scheduler");
-        return &hrns;
-    }
-    ischeduler* random_scheduler() {
-        spdlog::info("random_sche");
-        return &rss;
-    }
     ischeduler* round_robine_scheduler() {
         spdlog::info("round_robine_scheduler");
         return &rr;
