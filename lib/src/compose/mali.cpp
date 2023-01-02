5fa49bd7e29923b1e1160a36c63c6de8f4ea8328
Piorosen <aoikazto@gmail.com>
2023-01-03 03:24:19 +0900

    Revert "test mali"
    This reverts commit 215849548b68adccd97058da7e19296cd84b4972.

--- lib/src/compose/mali.cpp
+++ lib/src/compose/mali.cpp
@@ -22,27 +22,15 @@ void mali_engine::init(const std::string file, compose::model_info info) {
        printf("MALI!!\n\n\n");
     this->target = od::CreatePipeline(option);
 }
-
-       
-
-stbi_uc* data_mali = nullptr;
-
 void mali_engine::inference(const std::string image) {
-    this->is_inference = true;
     common::InferenceResults<float> results;
-    int width = 224, height = 224, c = 3;
-    
-    // stbi_uc* data = stbi_load(image.c_str(), &width, &height, &c, 0);
-    if (data_neon == nullptr) { 
-           data_neon = (stbi_uc*)malloc(224 * 224 * 3 * 4);
-    }
+    int width, height, c;
+       
+    stbi_uc* data = stbi_load(image.c_str(), &width, &height, &c, 0);
     // cout << width << " " << height << " " << c << "\n";
-    this->target->Inference((unsigned char*)data_neon, width * height * c, sizeof(float), results);
-    // free(data);
-    // stbi_image_free(data);
-    this->is_inference = false;
-    
+    this->target->Inference((unsigned char*)data, width * height * c, 1, results);
+    stbi_image_free(data);
 }
 void mali_engine::deinit() { 
-    free(data_neon);
+    
 }
