c0f57cba202eaf5d3ce71729ceb81154ff34fb70
Piorosen <chacha@udon.party>
2023-01-05 10:39:40 +0000

    순서 변경함.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 {
     auto eft = scheduler::instance()->first_come_first_served();
     // vector<string> models { "resnet50", "resnet1001", "mobilenet"  };
-    vector<string> models { "resnet101", "mobilenet", "resnet50", };
+    vector<string> models { "mobilenet", "resnet50", "resnet101" };
     eft->init(models, 0);
     printf("hahaha\n\n");
     eft->sequence(models);
